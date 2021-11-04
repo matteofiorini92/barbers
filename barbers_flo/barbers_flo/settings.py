a2f63a49a7338acccb34cc2b48a8f581e1147482
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-11-04 17:25:59 +0000

    feat: created home app and base template. Linked and tested base and index templates

--- barbers_flo/barbers_flo/settings.py
+++ barbers_flo/barbers_flo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%n7bw1b9pnj76_f7@9vb7_jm&s1ozw(1m5lc&6mjdpmd0!eq4$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'barbers_flo',
+    'home',
     'services'
 ]
 
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'barbers_flo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- barbers_flo/barbers_flo/views.py
+++ barbers_flo/barbers_flo/views.py
@@ -1,6 +1,1 @@
 from django.shortcuts import render
-
-
-def home(request):
-    """ A view to return the home page """
-    return render(request, 'barbers_flo/home.html')
