1edd41f04d80700ec18b2892791e11fda02bcc22
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-11-26 17:58:07 +0000

    feat: added webhooks listeners to stripe integration

--- barbers_flo/checkout/views.py
+++ barbers_flo/checkout/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
+
+import stripe
 
 # Create your views here.
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    total = 100.00
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+    print(intent)
     template = 'checkout/checkout.html'
     context = {
-        'stripe_public_key': 'pk_test_51K00ZGB6ftyYlm0uHpm82edhLqQwxPLLszWXvpnkDsLpnl1PKVWJxjmqWw5srGRzdy1RqI2Wi4yCcFtNdx49QglP00XHmO35en',
-        'client_secret': 'test_client_secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
     return render(request, template, context)
