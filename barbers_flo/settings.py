0d24cffe6dbe28b3560cb481d22a523392759683
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-12-07 16:47:28 +0000

    feat: removed secret key and set debug

--- barbers_flo/settings.py
+++ barbers_flo/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%n7bw1b9pnj76_f7@9vb7_jm&s1ozw(1m5lc&6mjdpmd0!eq4$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['barbers-flo.herokuapp.com', 'localhost']
 
