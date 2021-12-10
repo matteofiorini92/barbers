d178d12d72776413f95ed582439be2a3ea176e38
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-12-10 14:42:48 +0000

    feat: added list of reservation to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+from checkout.models import Reservation
 from django.contrib.auth.models import User
 from .forms import UserProfileForm, UserForm
 
@@ -12,6 +13,8 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
     user = get_object_or_404(User, username=request.user)
+    # get 5 most recent reservations
+    reservations = Reservation.objects.filter(user_profile=profile).order_by('-date')[:5]
     if request.method == 'POST':
         form_profile = UserProfileForm(request.POST, instance=profile)
         if form_profile.is_valid():
@@ -28,7 +31,8 @@ def profile(request):
     context = {
         'form_profile': form_profile,
         'form_user': form_user,
-        'on_profile_page': True
+        'on_profile_page': True,
+        'reservations': reservations,
     }
 
     return render(request, template, context)
