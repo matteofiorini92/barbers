f11544e82ef8aa812669e9919d409dd452735ae4
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-12-15 16:26:03 +0000

    feat: barbers in booking system are now ordered by name

--- booking/views.py
+++ booking/views.py
@@ -74,7 +74,7 @@ def new_booking_2(request, treatment_id):
 def new_booking_3(request, treatment_id, day):
     """ A view to return the third step of the booking page (select barber) """
     treatment = get_object_or_404(Treatment, id=treatment_id)
-    barbers = Barber.objects.all()
+    barbers = Barber.objects.all().order_by('barber_name')
     print(barbers[0].profile_picture.url)
     template = 'booking/new_booking_3.html'
     context = {
@@ -85,45 +85,10 @@ def new_booking_3(request, treatment_id, day):
     return render(request, template, context)
 
 
-
-#def make_availabilities():
-#    """ A view to create new slots for the end of the calendar """
-#    last_available_day = Availability.objects.all().order_by('-date')[:1][0].date
-#    print(last_available_day)
-#    today = date.today()
-#    days_curr_month = monthrange(today.year, today.month)[1]
-#    one_month_from_now = today + timedelta(days=days_curr_month)
-#    barbers = Barber.objects.all()
-#
-#    if (last_available_day != one_month_from_now):
-#        for single_date in daterange(last_available_day, one_month_from_now + timedelta(days=1)):
-#            if single_date.weekday() != 5 and single_date.weekday() != 6:
-#                for barber in barbers:
-#                    start_time = datetime.strptime(str(single_date) + " " + str(barber.start_time), '%Y-%m-%d %H:%M:%S')
-#                    end_time = datetime.strptime(str(single_date) + " " + str(barber.end_time), '%Y-%m-%d %H:%M:%S')
-#                    for thirty_minutes in timerange(start_time, end_time):
-#                        slot = Availability(barber=barber, date=thirty_minutes.date(), time=thirty_minutes.time(), available=True)
-#                        slot.save()
-
-
-#def clean_up_availabilities():
-#    """ A view to delete availability for past dates """
-#    oldest_available_date = Availability.objects.all().order_by('date')[:1][0].date
-#    today = date.today()
-#    if oldest_available_date < today:
-#        availabilities = Availability.objects.all().order_by('date')
-#        for availability in availabilities:
-#            if availability.date < today:
-#                availability.delete()
-#            else:
-#                break
-
 def new_booking_4(request, treatment_id, day, barber_id):
     """ A view to return the third fourth of the booking page (select time) """
     treatment = get_object_or_404(Treatment, id=treatment_id)
     barber = get_object_or_404(Barber, id=barber_id)
-#    clean_up_availabilities()
-#    make_availabilities()
     availabilities = Availability.objects.filter(barber=barber, date=day, available=True)
     if not availabilities:
         print(availabilities)
