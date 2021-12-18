7be4ab0585e06273c9e5bcf0a14d76aedc722697
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-12-18 18:58:07 +0000

    feat: added 404 custom template

--- barbers_flo/views.py
+++ barbers_flo/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+
+# https://docs.djangoproject.com/en/4.0/ref/views/#django.views.defaults.page_not_found
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
